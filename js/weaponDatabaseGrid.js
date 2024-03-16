f0678cdb670cba5f14cf9a03fd0278c1f847c133
BBarricklow <BLBarricklow@gmail.com>
2024-03-15 21:31:43 -0700

    Refine database grid and data insertion into grid

--- js/weaponDatabaseGrid.js
+++ js/weaponDatabaseGrid.js
@@ -1,13 +1,24 @@
 function displayAllWeapons() {
     console.log("Hi")
-    let targetColName = document.getElementById('weapon-names');
-    let targetColSub = document.getElementById('weapon-subs');
-    let targetColSpecial = document.getElementById('weapon-specials');
+    // let targetColName = document.getElementById('weapon-names');
+    // let targetColSub = document.getElementById('weapon-subs');
+    // let targetColSpecial = document.getElementById('weapon-specials');
+    // for (let i = 0; i < weapons.length; i++) {
+    //     targetColName.innerHTML += `<span class="cell">${weapons[i].name}</span>`
+    //     // console.log(weapons[i].name)
+    //     targetColSub.innerHTML += `<span class="cell">${weapons[i].subWeapon}</span>`
+    //     targetColSpecial.innerHTML += `<span class="cell">${weapons[i].specialWeapon}</span>`
+    // }
+    let mainGrid = document.getElementById('main-grid');
     for (let i = 0; i < weapons.length; i++) {
-        targetColName.innerHTML += `<span class="cell">${weapons[i].name}</span>`
-        // console.log(weapons[i].name)
-        targetColSub.innerHTML += `<span class="cell">${weapons[i].subWeapon}</span>`
-        targetColSpecial.innerHTML += `<span class="cell">${weapons[i].specialWeapon}</span>`
+        mainGrid.innerHTML += `<div class="row">
+                                 <span class="cell2">${weapons[i].name}</span>
+                                 <img class="small-icon" src="${weapons[i].icon}">
+                                 <span class="cell2">${weapons[i].subWeapon}</span>
+                                 <img class="small-icon" src="${weapons[i].subIcon}"</span>
+                                 <span class="cell2">${weapons[i].specialWeapon}</span>
+                                 <img class="small-icon" src="${weapons[i].specialIcon}"</span>
+                               </div>`
     }
 }
 
